5292435c8a7aa3b44aa2432b4761175e97229351
felrichart-perso <fel.richart@gmail.com>
2025-07-14 17:06:25 +0200

    build: add alias for src

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   base: './',
   plugins: [tailwindcss()],
+  resolve: {
+    alias: {
+      '@': resolve.resolve(__dirname, './src'),
+    },
+  },
 });
